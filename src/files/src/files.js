e0a98d68e13a9659eddc04a03a0865eaa5c705e3
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-05-07 15:54:53 +0300

    feature/WGRND-576 add Files.upload

--- src/files/src/files.js
+++ src/files/src/files.js
@@ -1,43 +1,18 @@
 /* eslint-disable no-restricted-syntax */
 const ipfsClient = require('ipfs-http-client');
-const BufferList = require('bl');
 
 class Files {
 
   /**
-   * @param uri {string} - IPFS API server uri
+   * @param uri {string|object} - IPFS API server
    */
   constructor(uri) {
     this.node = ipfsClient(uri);
   }
 
-  async add(path, content) {
-    const chunks = await this.node.files.write(path, content);
-    let cid;
-    for await (const chunk of chunks) {
-      cid = chunk.cid; // return cid from last (and only one) chunk
-    }
-    return cid.toString();
+  async upload(path, content) {
+    await this.node.files.write(path, Buffer.from(content), { create: true });
   }
-
-  /**
-   * @param uri {string} CID of the file with claim content
-   */
-  // async get(uri: string): Promise<string> {
-  //   let claim = '';
-  //   for await (const file of this.node.get(`/ipfs/${uri}`)) {
-  //     const content = new BufferList();
-  //     for await (const chunk of file.content) {
-  //       content.append(chunk);
-  //     }
-  //     claim += content.toString();
-  //   }
-  //   return claim;
-  // }
-
-  // async delete(uri: string): Promise<boolean> {
-  //   throw new Error('Not supported by IPFS');
-  // }
 }
 
 module.exports.Files = Files;
--- src/files/test/files.test.js
+++ src/files/test/files.test.js
@@ -1,43 +1,37 @@
-/* eslint-disable func-names */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 const { expect } = require('chai');
-const fs = require('fs');
-// import { shutDownIpfsDaemon, spawnIpfsDaemon } from '../../../tests';
+const { shutDownIpfsDaemon, spawnIpfsDaemon } = require('./testIpfsDaemon');
 const { Files } = require('../src');
 
 describe('[FILES]', function () {
   this.timeout(0);
+  let files;
+  let api;
 
-  // before('start ipfs daemon', async function () {
-  //   const api = await spawnIpfsDaemon();
-  //   this.store = new DidStore(api);
-  // });
+  before('start ipfs daemon', async function () {
+    // api = '/ip4/127.0.0.1/tcp/5001'; // connect to existed node API
+    api = await spawnIpfsDaemon();
+  });
 
-  it.only('should connect to local ipfs node', async () => {
-    const apiUrl = '/ip4/127.0.0.1/tcp/5001';
-    const files = new Files(apiUrl);
+  it('should connect to local ipfs node', async () => {
+    files = new Files(api);
     const { version } = await files.node.version();
-    // console.log('>>> ipfs verison: ', version);
     expect(version).not.null;
-  })
-
-  it('get() should fetch claim by uri returned from save()', async function () {
-    const claim = 'TEST CLAIM';
-    const cid = await this.store.save(claim);
-    const stored = await this.store.get(cid);
-    expect(stored).equal(claim);
   });
 
-  it('get() should fetch a big claim by uri returned from save()', async function () {
-    const claim = fs.readFileSync('./test/big-claim.txt').toString('utf8');
-    const cid = await this.store.save(claim);
-    const stored = await this.store.get(cid);
-    expect(stored).equal(claim);
+  it('should create a new folder', async () => {
+    const filePath = '/root';
+    const content = 'Hello IPFS';
+    await files.upload(filePath, content);
+    const chunks = [];
+    for await (const chunk of files.node.files.read(filePath)) {
+      chunks.push(chunk);
+    }
+    expect(Buffer.concat(chunks).toString()).equal(content);
   });
 
-  // after('stop the daemon', async () => {
-  //   await shutDownIpfsDaemon();
-  // });
+  after('stop the daemon', async () => {
+    await shutDownIpfsDaemon();
+  });
 });
 
 

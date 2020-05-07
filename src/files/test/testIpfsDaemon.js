8eecfa2f2e9828e887ccc4cfd3615783af0b3819
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-05-07 16:04:12 +0300

    feature/WGRND-555 remove unrelated to task

--- src/files/test/testIpfsDaemon.js
+++ src/files/test/testIpfsDaemon.js
@@ -1,17 +1,19 @@
-import Ctl from 'ipfsd-ctl';
-import path from 'path';
-import ipfsHttpModule from 'ipfs-http-client';
+const Ctl = require('ipfsd-ctl');
+const path = require('path');
+const ipfsHttpModule = require('ipfs-http-client');
 
 let ipfsd;
 
-export async function spawnIpfsDaemon() {
-  const ipfsBin = path.resolve(__dirname, '../', 'node_modules/.bin', 'jsipfs');
+async function spawnIpfsDaemon() {
+  const ipfsBin = path.resolve(__dirname, '../../../', 'node_modules/.bin', 'jsipfs');
   ipfsd = await Ctl.createController({
     type: 'js', disposable: true, test: true, ipfsBin, ipfsHttpModule,
   });
   return ipfsd.apiAddr;
 }
 
-export async function shutDownIpfsDaemon() {
+async function shutDownIpfsDaemon() {
   return ipfsd && ipfsd.stop();
 }
+
+module.exports = { spawnIpfsDaemon, shutDownIpfsDaemon };

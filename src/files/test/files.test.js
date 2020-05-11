7defb22cc6f0772444686e2fa155a2cc9be88486
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2020-05-11 08:16:56 +0300

    feature/WGRND-576 instantiate files in before

--- src/files/test/files.test.js
+++ src/files/test/files.test.js
@@ -10,15 +10,15 @@ describe('[FILES]', function () {
   before('start ipfs daemon', async function () {
     // api = '/ip4/127.0.0.1/tcp/5001'; // connect to existed node API
     api = await spawnIpfsDaemon();
+    files = new Files(api);
   });
 
-  it.only('should connect to local ipfs node', async () => {
-    files = new Files(api);
+  it('should connect to local ipfs node', async () => {
     const { version } = await files.node.version();
     expect(version).not.null;
   });
 
-  it.only('should upload file ', async () => {
+  it('should upload file ', async () => {
     let content = 'Hello';
     const path = await files.upload(content);
     expect(await files.getFileByHash(path)).equal(content);

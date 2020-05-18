fc930a2229c6d9df0bab14ab386923b0dcc1ac84
richardowsky <brooklyn.od.ua@gmail.com>
2020-05-18 17:23:53 +0300

    bugfix auth, register, logout, update user

--- src/database/initdb.js
+++ src/database/initdb.js
@@ -4,36 +4,36 @@ const configDB = require('./configDB');
 
 
 async function initdb() {
-    const conn = connection(configDB);
-    await dbrequestor.query(conn, 'SET default_tablespace = \'\';');
-    await dbrequestor.query(conn, 'DROP TABLE IF EXISTS certs');
-    await dbrequestor.query(conn, 'DROP TABLE IF EXISTS users');
-    await dbrequestor.query(conn, 'DROP TABLE IF EXISTS folders');
-    await dbrequestor.query(conn, 'DROP TABLE IF EXISTS files');
-    await dbrequestor.query(conn, `CREATE TABLE public.certs (username character varying(100), cert character varying(2048));`);
-    await dbrequestor.query(conn, 'ALTER TABLE public.certs OWNER TO admin;');
-    await dbrequestor.query(conn, `CREATE TABLE public.users (
-      username character varying(100) NOT NULL,
-      email character varying(100) NOT NULL,
-      password character varying(70),
-      folder character varying(100) );`);
-    await dbrequestor.query(conn, `CREATE TABLE public.folders (
-      name character varying(100) NOT NULL,
-      hash character varying(100) NOT NULL,
-      parentHash character varying(100),
-      folders character varying(4096),
-      files character varying(4096) );`);
-    await dbrequestor.query(conn, `CREATE TABLE public.files (
-      name character varying(100) NOT NULL,
-      hash character varying(100) NOT NULL,
-      type character varying(100) NOT NULL,
-      parentHash character varying(100));`);
-    await dbrequestor.query(conn, 'ALTER TABLE public.users OWNER TO admin;');
-    await dbrequestor.query(conn, 'ALTER TABLE public.folders OWNER TO admin;');
-    await dbrequestor.query(conn, 'ALTER TABLE public.files OWNER TO admin;');
-    await dbrequestor.query(conn, 'ALTER TABLE ONLY public.users ADD CONSTRAINT users_pkey PRIMARY KEY (username);');
-    await dbrequestor.query(conn, 'ALTER TABLE ONLY public.certs ADD CONSTRAINT certs_name_fkey FOREIGN KEY (username) REFERENCES public.users(username);');
-    await dbrequestor.query(conn, "INSERT INTO public.users VALUES ('user1', 'password', 'email@mail.com')");
+    // const conn = connection(configDB);
+    // await dbrequestor.query(conn, 'SET default_tablespace = \'\';');
+    // await dbrequestor.query(conn, 'DROP TABLE IF EXISTS certs');
+    // await dbrequestor.query(conn, 'DROP TABLE IF EXISTS users');
+    // await dbrequestor.query(conn, 'DROP TABLE IF EXISTS folders');
+    // await dbrequestor.query(conn, 'DROP TABLE IF EXISTS files');
+    // await dbrequestor.query(conn, `CREATE TABLE public.certs (username character varying(100), cert character varying(2048));`);
+    // await dbrequestor.query(conn, 'ALTER TABLE public.certs OWNER TO admin;');
+    // await dbrequestor.query(conn, `CREATE TABLE public.users (
+    //   username character varying(100) NOT NULL,
+    //   email character varying(100) NOT NULL,
+    //   password character varying(70),
+    //   folder character varying(100) );`);
+    // await dbrequestor.query(conn, `CREATE TABLE public.folders (
+    //   name character varying(100) NOT NULL,
+    //   hash character varying(100) NOT NULL,
+    //   parentHash character varying(100),
+    //   folders character varying(4096),
+    //   files character varying(4096) );`);
+    // await dbrequestor.query(conn, `CREATE TABLE public.files (
+    //   name character varying(100) NOT NULL,
+    //   hash character varying(100) NOT NULL,
+    //   type character varying(100) NOT NULL,
+    //   parentHash character varying(100));`);
+    // await dbrequestor.query(conn, 'ALTER TABLE public.users OWNER TO admin;');
+    // await dbrequestor.query(conn, 'ALTER TABLE public.folders OWNER TO admin;');
+    // await dbrequestor.query(conn, 'ALTER TABLE public.files OWNER TO admin;');
+    // await dbrequestor.query(conn, 'ALTER TABLE ONLY public.users ADD CONSTRAINT users_pkey PRIMARY KEY (username);');
+    // await dbrequestor.query(conn, 'ALTER TABLE ONLY public.certs ADD CONSTRAINT certs_name_fkey FOREIGN KEY (username) REFERENCES public.users(username);');
+    // await dbrequestor.query(conn, "INSERT INTO public.users VALUES ('user1', 'password', 'email@mail.com')");
 }
 module.exports = initdb;
 
--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,18 +1,22 @@
 "use strict";
 
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
 const jwt = require("jsonwebtoken");
 const issuer = "482solutions";
+const {InMemoryWallet, X509WalletMixin, Gateway} = require('fabric-network');
 
 exports.verifyToken = function (req, authOrSecDef, token, callback) {
     function sendError() {
-        return {code: 404, payload: 'Invalid token supplied.'};
+        return {code: 404, payload: {message: 'Invalid token supplied.'}};
     }
 
     if (token && token.indexOf("Bearer ") === 0) {
         const tokenString = token.split(" ")[1];
         jwt.verify(tokenString, "482solutions", function (verificationError, decodedToken) {
             if (verificationError === null && decodedToken) {
-                const issuerMatch = decodedToken.issuer === issuer;
+                const issuerMatch = decodedToken.issuer === issuer; return {code: 404, payload: {message: 'Invalid token supplied.'}};
                 if (issuerMatch) {
                     req.auth = decodedToken;
                     return callback(null);
@@ -48,7 +52,62 @@ exports.validationCSR = (string) => {
 exports.getUserFromToken = async (token) => {
     if (token && token.indexOf("Bearer ") === 0) {
         const tokenString = token.split(" ")[1];
-        const decodedToken = await jwt.verify(tokenString, "482solutions".toString('base64'))
+        if (tokenString.length < 5){
+            return null
+        }
+        const decodedToken = await jwt.verify(tokenString, "482solutions".toString('base64'), )
         return decodedToken.data
+    }
+}
+
+exports.sendTransaction = async ({ identity, transaction, network: networkOptions }) => {
+    const wallet = new InMemoryWallet();
+    const mixin = X509WalletMixin.createIdentity(identity.mspId, identity.certificate, identity.privateKey);
+    await wallet.import(identity.label, mixin);
+
+    // A gateway defines the peers used to access Fabric networks
+    const gateway = new Gateway();
+
+    // Main try/catch block
+    try {
+        // Load connection profile; will be used to locate a gateway
+        let connectionProfile = yaml.safeLoad(
+            fs.readFileSync(path.resolve(__dirname, process.env.FABRIC_CONFIG_FILE), 'utf8')
+        );
+
+        // Set connection options; identity and wallet
+        let connectionOptions = {
+            identity: identity.label,
+            wallet,
+            discovery: { enabled: false, asLocalhost: true }
+        };
+
+        // Connect to gateway using application specified parameters
+        await gateway.connect(connectionProfile, connectionOptions);
+        console.log('Connect to Fabric gateway');
+
+        const network = await gateway.getNetwork(networkOptions.channel);
+        console.log('Use network channel:', networkOptions.channel);
+
+        const contract = await network.getContract(networkOptions.chaincode, networkOptions.contract);
+        console.log(`Use ${networkOptions.contract} smart contract of chaincode ${networkOptions.chaincode}.`);
+
+        const issueResponse = await contract.submitTransaction(transaction.name, ...transaction.props);
+        console.log('Submit transaction:', transaction.name, 'with props:', transaction.props);
+
+        gateway.disconnect();
+        console.log('Disconnect from Fabric gateway.');
+
+        return JSON.parse(issueResponse.toString());
+
+    } catch (error) {
+
+        console.log(`Error processing transaction. ${error}`);
+        console.log(error.stack);
+
+        // Disconnect from the gateway
+        gateway.disconnect();
+        console.log('Disconnect from Fabric gateway.');
+
     }
 }
